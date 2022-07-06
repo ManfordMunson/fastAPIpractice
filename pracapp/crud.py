6af322947ce585ffbfe8192ddb9c2085f1203040
ManfordMunson <masonmunford@gmail.com>
2022-07-06 16:23:48 -0500

    added more functions and db tables

--- pracapp/crud.py
+++ pracapp/crud.py
@@ -1,13 +1,17 @@
+from importlib.util import module_for_loader
 from pyexpat import model
+from statistics import mode
 from unicodedata import name
 from unittest import skip
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+#Returns entire Dogs table
 def get_dog(db: Session):
     return db.query(models.Dog).all()
 
+#Adds a dog to the Dogs table 
 def create_dog(db: Session, dog: schemas.DogCreate):
     db_dog = models.Dog(name=dog.name, owner=dog.owner, age=dog.age, breed=dog.breed)
     db.add(db_dog)
@@ -15,12 +19,41 @@ def create_dog(db: Session, dog: schemas.DogCreate):
     db.refresh(db_dog)
     return db_dog
 
-def get_owner(db: Session,name:str):
-    return db.query(models.Dog).all()
+#Returns entire Owners table
+def get_owner(db: Session):
+    return db.query(models.Owner).all()
 
+#Adds a owner to the Owners tabls
 def create_owner(db: Session, owner: schemas.OwnerCreate):
     db_owner = models.Owner(name=owner.name, paid=owner.paid)
     db.add(db_owner)
     db.commit()
     db.refresh(db_owner)
     return db_owner
+
+#Joins Dogs and Owners tables where Owner.name == Dog.owner
+def get_dog_owner(db:Session):
+    db_dog_and_owner = db.query(models.Owner, models.Dog).filter(models.Owner.name == models.Dog.owner).all()
+    return db_dog_and_owner
+
+def create_payment(db: Session, payment: schemas.PayCreate):
+    db_payment = models.Payments(name=payment.name, amount=payment.amount)
+    db.add(db_payment)
+    db.commit()
+    db.refresh(db_payment)
+    return db_payment
+
+def get_payment(db:Session):
+    return db.query(models.Payments).all()
+
+def get_payment_by_name(db:Session, user_name:str):
+    return db.query(models.Payments).filter(user_name == models.Payments.name).all()
+
+def get_all_data_by_name(db:Session, user_name:str):
+    return db.query(models.Payments, models.Owner, models.Dog).filter(
+        models.Owner.name == user_name,
+        models.Payments.name == user_name,
+        models.Dog.owner == user_name).all()
+
+def get_all(db:Session):
+    return (db.query(models.Owner).all(), db.query(models.Dog).all(), db.query(models.Payments).all())
--- pracapp/models.py
+++ pracapp/models.py
@@ -1,8 +1,10 @@
-from sqlalchemy import BOOLEAN, Boolean, Column, ForeignKey, Integer, String
+from xmlrpc.client import DateTime
+from sqlalchemy import Boolean, Column, DateTime, Float, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
+#Creates the Dog table
 class Dog(Base):
     __tablename__ = "dogs"
     id = Column(Integer, primary_key = True, index =True)
@@ -13,11 +15,17 @@ class Dog(Base):
 
     #owners = relationship("Owner", back_populates="dogs")
 
-
+#Creates the Owner table 
 class Owner(Base):
     __tablename__ = "owner"
     id = Column(Integer, primary_key = True, index = True)
     name = Column(String)
-    paid = Column(BOOLEAN)
+    paid = Column(Boolean)
     
     #dogs = relationship("Dog", back_populates="owner")
+
+class Payments(Base):
+    __tablename__ = "payments"
+    id = Column(Integer, primary_key = True, index=True)
+    name = Column(String)
+    amount = Column(Float)
